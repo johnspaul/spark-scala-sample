976796ce92ea5264a31bb5745eee06d262322a31
johns_keyvalue <johns@keyvalue.systems>
2021-07-24 18:54:04 +0530

    Modification to list students who didn't write exam

--- src/main/scala/MarkCalculatorApp.scala
+++ src/main/scala/MarkCalculatorApp.scala
@@ -16,16 +16,22 @@ object MarkCalculatorApp {
         .getOrCreate()
       import spark.implicits._
       val studentMarksDF = spark.read.textFile(studentMarkFile).map(_.split("\\s+")).map{ attributes =>
-        val marks = List(attributes(1).toInt, attributes(2).toInt, attributes(3).toInt, attributes(4).toInt, attributes(5).toInt, attributes(6).toInt)
-        val total: Float = attributes(1).toInt + attributes(2).toInt +  attributes(3).toInt + attributes(4).toInt + attributes(5).toInt +  attributes(6).toInt
+        val subject1 = attributes(1).toInt
+        val subject2 = attributes(2).toInt
+        val subject3 = attributes(3).toInt
+        val subject4 = attributes(4).toInt
+        val subject5 = attributes(5).toInt
+        val subject6 = attributes(6).toInt
+        val marks = List(subject1, subject2, subject3, subject4, subject5, subject6)
+        val total: Float = marks.sum
         val percentage: Float = (total / 600) * 100
         val pass: String = if(marks.exists(x => x < 40)) "F" else  "P"
-        StudentMark(attributes(0).toInt, attributes(1).toInt, attributes(2).toInt, attributes(3).toInt, attributes(4).toInt, attributes(5).toInt, attributes(6).toInt, total, percentage, attributes(7).toInt, pass)
+        StudentMark(attributes(0).toInt, subject1, subject2, subject3, subject4, subject5, subject6, total, percentage, attributes(7).toInt, pass)
       }.toDF()
       val studentNameDF = spark.read.textFile(studentNameFile).map(_.split("\\s+")).map(s => Student(s(0).toInt, s(1))).toDF()
       studentNameDF.createOrReplaceTempView("Students")
       studentMarksDF.createOrReplaceTempView("StudentMarks")
-      val response = spark.sql("SELECT stdm.id, std.name total, percentage, semester, result FROM StudentMarks as stdm, Students AS std WHERE std.id = stdm.id  ORDER BY semester ASC, total DESC ")
+      val response = spark.sql("SELECT std.id, std.name, COALESCE(total,0), COALESCE(percentage,0), semester, COALESCE(result, 'F') FROM StudentMarks as stdm RIGHT JOIN Students AS std ON std.id = stdm.id  ORDER BY semester ASC, total DESC ")
       response.toJavaRDD.coalesce(1,false).saveAsTextFile("src/main/resources/output.txt")
       spark.stop()
     }
